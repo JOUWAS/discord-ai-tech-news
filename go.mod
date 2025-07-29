5365dfdf62f31a815bca2c425e7fa1b0dcf9b319
Muh Zaqi <zaqiikhwan22@gmail.com>
2025-07-29 18:44:52 +0700

    feat: init response when get message in discord

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module discord-ai-tech-news/main
 go 1.23.5
 
 require (
+	github.com/bwmarrin/discordgo v0.29.0 // indirect
 	github.com/bytedance/sonic v1.14.0 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
@@ -14,6 +15,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/gin-gonic/gin"
+	// "github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
@@ -14,12 +18,58 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	port := os.Getenv("APP_PORT")
+	token := os.Getenv("TOKEN")
+	if token == "" {
+		log.Fatal("TOKEN is not set in the environment variables")
+	}
 
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+	log.Println("TOKEN is set to:", token)
+
+	sess, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatalf("error creating Discord session: %v", err)
+	}
+
+	sess.AddHandler(func(s *discordgo.Session, r *discordgo.MessageCreate) {
+		if r.Author.ID == s.State.User.ID {
+			return
+		}
+
+		log.Printf("=== Message Debug Info ===")
+		log.Printf("Author: %s (ID: %s)", r.Author.Username, r.Author.ID)
+		log.Printf("Content: '%s' (length: %d)", r.Content, len(r.Content))
+		log.Printf("Channel: %s", r.ChannelID)
+		log.Printf("Message ID: %s", r.ID)
+		log.Printf("========================")
+
+		if r.Content == "hello" {
+			_, err := s.ChannelMessageSend(r.ChannelID, "sini kau hanip!!!")
+			if err != nil {
+				log.Printf("error sending message: %v", err)
+			}
+		}
 	})
 
-	r.Run(":" + port)
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = sess.Open()
+	if err != nil {
+		log.Fatalf("error opening connection: %v", err)
+	}
+	defer sess.Close()
+
+	log.Println("Bot is now running. Press CTRL+C to exit.")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, os.Interrupt, syscall.SIGTERM)
+	<-sc
+
+	// port := os.Getenv("APP_PORT")
+
+	// r := gin.Default()
+	// r.GET("/", func(c *gin.Context) {
+	// 	c.String(200, "Hello, World!")
+	// })
+
+	// r.Run(":" + port)
 }
