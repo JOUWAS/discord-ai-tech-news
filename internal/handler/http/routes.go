1e76b392cd796c2d3e69c5008ef331b616fc08db
Muh Zaqi <zaqiikhwan22@gmail.com>
2025-08-05 00:19:34 +0700

    feat: test cron job for send news in WIB local time

--- internal/handler/http/routes.go
+++ internal/handler/http/routes.go
@@ -21,13 +21,16 @@ func RegisterRoutes(r *gin.Engine) {
 		c.JSON(http.StatusOK, gin.H{
 			"status": "running",
 			"cron_jobs": gin.H{
-				"morning_news":   "08:00 daily",
-				"afternoon_news": "13:00 daily",
-				"evening_news":   "17:00 daily",
+				"morning_news":   "08:00 WIB (02:00 Frankfurt)",
+				"afternoon_news": "13:00 WIB (07:00 Frankfurt)",
+				"evening_news":   "17:00 WIB (11:00 Frankfurt)",
+				"test_news":      "00:25 WIB (18:25 Frankfurt)",
 				"service_health": "every minute",
 			},
-			"timezone":   "Asia/Jakarta (WIB)",
-			"last_check": time.Now().Format("2006-01-02 15:04:05 WIB"),
+			"deployment_timezone": "Frankfurt (GMT+1/GMT+2)",
+			"target_timezone":     "Asia/Jakarta (GMT+7/WIB)",
+			"timezone_note":       "Cron times adjusted for GMT+7 target from Frankfurt deployment",
+			"last_check":          time.Now().Format("2006-01-02 15:04:05 MST"),
 		})
 	})
 
